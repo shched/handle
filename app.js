b9b6199663c2647786d5206a3d4d0c2808c09a95
shched <v-shchedrinsky@ya.ru>
2016-05-10 01:21:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.configure(function() {
-    app.engine('html', require('uinexpress').__express)  // Используем функцию "template" библиотеки underscore для рендеринга
-    app.set('view engine', 'html')                      
+    app.engine('html', require('uinexpress').__express)    // Используем функцию "template" библиотеки underscore для рендеринга
+    app.set('view engine', 'html')
     app.set('views', __dirname + "/tpl");
-    app.set("view options", {layout: 'layout.html'});   // Файл layout.html по умолчанию будет оборачивать все шаблоны
-    app.use(express.static(__dirname + "/public"));      // Делаем файлы из папки public доступными на сайте
+    app.set("view options", {layout: 'layout.html'});    // Файл layout.html по умолчанию будет оборачивать все шаблоны
+    app.use(express.static(__dirname + "/public"));    // Делаем файлы из папки public доступными на сайте
 });
 
-app.get('/', function(req, res){           // Обрабатываем запрос корневой страницы "/"
+app.get('/', function(req, res){    // Обрабатываем запрос корневой страницы "/"
     res.render('index.html');
 });
 
@@ -41,12 +41,6 @@ app.get('/err404', function(req, res){    // Обрабатываем запро
     res.render('err404.html');
 });
 
-var connect = require("connect");
-
-var app = connect.createServer().use(connect.static(__dirname + '/public'));
-
-app.listen(port);
-
-var port = process.env.PORT || 5000;       
-app.listen(port)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
-console.log("Listening at " + port)        // Пишем в консоль, что запустились
+var port = process.env.PORT || 5000;
+app.listen(port)    // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
+console.log("Listening at " + port)    // Пишем в консоль, что запустились
