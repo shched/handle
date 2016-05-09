7a9e20ff715eb2984201401bed1335dee09b4475
shched <v-shchedrinsky@ya.ru>
2016-05-09 17:30:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ app.configure(function() {
     app.engine('html', require('uinexpress').__express) // Используем функцию "template" библиотеки underscore для рендеринга
     app.set('view engine', 'html')                      
     app.set('views', __dirname + "/tpl");
+    app.set("view options", {layout: 'layout.html'});   // Файл layout.html по умолчанию будет оборачивать все шаблоны
+    app.use(express.static(__dirname + "/public"));     // Делаем файлы из папки public доступными на сайте
 });
 
 app.get('/', function(req, res){          // Обрабатываем запрос корневой страницы "/"
     res.render('index.html');
 });
-app.get('/news', function(req, res){ // Обрабатываем запрос страницы "/news"
-    res.render('news.html');
+app.get('/portfolio', function(req, res){ // Обрабатываем запрос страницы "/portfolio"
+    res.render('portfolio.html');
 });
 
 var port = process.env.PORT || 5000;       
