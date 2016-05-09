d64246d176d409134a347df8b3d8a258b3783301
shched <v-shchedrinsky@ya.ru>
2016-05-09 17:50:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,43 @@ var express = require('express');
 var app = express();
 
 app.configure(function() {
-    app.engine('html', require('uinexpress').__express) // Используем функцию "template" библиотеки underscore для рендеринга
+    app.engine('html', require('uinexpress').__express)  // Используем функцию "template" библиотеки underscore для рендеринга
     app.set('view engine', 'html')                      
     app.set('views', __dirname + "/tpl");
-    app.set("view options", {layout: 'history.html'});   // Файл layout.html по умолчанию будет оборачивать все шаблоны
-    app.use(express.static(__dirname + "/public"));     // Делаем файлы из папки public доступными на сайте
+    app.set("view options", {layout: 'history.html'});   // Файл history.html по умолчанию будет оборачивать все шаблоны
+    app.use(express.static(__dirname + "/public"));      // Делаем файлы из папки public доступными на сайте
 });
 
-app.get('/', function(req, res){          // Обрабатываем запрос корневой страницы "/"
+app.get('/', function(req, res){           // Обрабатываем запрос корневой страницы "/"
     res.render('index.html');
 });
-app.get('/history', function(req, res){ // Обрабатываем запрос страницы "/portfolio"
+
+app.get('/history', function(req, res){    // Обрабатываем запрос страницы "/history"
     res.render('history.html');
+});
+
+app.get('/news', function(req, res){    // Обрабатываем запрос страницы "/news"
+    res.render('news.html');
+});
+
+app.get('/abit', function(req, res){    // Обрабатываем запрос страницы "/abit"
+    res.render('abit.html');
+});
+
+app.get('/stud', function(req, res){    // Обрабатываем запрос страницы "/stud"
+    res.render('stud.html');
+});
+
+app.get('/links', function(req, res){    // Обрабатываем запрос страницы "/links"
+    res.render('links.html');
+});
+
+app.get('/about', function(req, res){    // Обрабатываем запрос страницы "/about"
+    res.render('about.html');
+});
+
+app.get('/err404', function(req, res){    // Обрабатываем запрос страницы "/err404"
+    res.render('err404.html');
 });
 
 var port = process.env.PORT || 5000;       
